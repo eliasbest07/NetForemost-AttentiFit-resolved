44eb79404345d7859aae5f0da25cde799ec09605
EliasMontilla <eliasbest634@gmail.com>
2023-03-08 13:08:37 -0500

    fixes splash screen

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
-   
   const SplashScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    pasarPantalla(context);
+    return Scaffold(
       body: Column(
-        mainAxisAlignment:MainAxisAlignment.center ,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('AttentiFit',style: TextStyle(fontSize: 26),),
-          const SizedBox(height: 25,),
+          const Text(
+            'AttentiFit',
+            style: TextStyle(fontSize: 26),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
           Center(
-             child: Image.asset('assets/logo.png'),
+            child: Image.asset('assets/logo.png'),
           ),
         ],
       ),
     );
+  }
+
+  void pasarPantalla(BuildContext context) async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacementNamed(context, 'home');
   }
 }
